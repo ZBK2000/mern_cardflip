11f2c472c76b077a201a10f583d81ea4ba2ed0ef
Zupko Balazs <you@example.com>
2023-01-18 17:37:00 +0100

    adding the ability to create, delete deck, go to decks page

---
+++ client/src/Deck.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Deck(){return(
+      <h1>hello</h1>)
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,16 +2,22 @@ import express, {Request, Response} from "express"
 import mongoose from "mongoose"
 import Deck from "../models/Deck"
 import { config } from "dotenv"
+import cors from "cors"
 config()
 
 
 const PORT = 5000
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.get("/",(req: Request, res: Response)=>{
     res.send("hello world")
     
+})
+app.get("/decks", async (req: Request, res: Response)=>{
+    const decks = await Deck.find()
+    res.json(decks)
 })
 
 app.post("/decks", async (req: Request, res: Response)=>{
@@ -20,6 +26,12 @@ app.post("/decks", async (req: Request, res: Response)=>{
     })
     const createDeck = await newDeck.save()
     res.json(createDeck)
+})
+
+app.delete("/decks/:deckId",  async (req: Request, res: Response)=>{
+const deckId = req.params.deckId
+const deck = await Deck.findByIdAndDelete(deckId)
+res.json(deck)
 })
 
 mongoose.connect(process.env.MONGO_URL!).then(()=>{
